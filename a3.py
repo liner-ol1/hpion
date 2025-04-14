9d9efd7d17453fbba2d341ee20bc13513edd1767
liner-ol1 <lizachukchaiolqwer@mail.ru>
2025-04-14 14:35:55 +0500

    Update a3.py

--- a3.py
+++ a3.py
@@ -3,6 +3,5 @@ count = 0
 for char in sentence:
     if ('А' <= char <= 'Я') or ('а' <= char <= 'я'):
         count += 1
-print("Количество букв в предложении:", count)
 
 #считает буквы в предложении
