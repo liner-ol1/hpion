b12370518da46419000bdfff4570bb3f6b6f94b5
liner-ol1 <lizachukchaiolqwer@mail.ru>
2025-04-21 16:21:51 +0500

    a3.py

--- a3.py
+++ a3.py
@@ -1,9 +1,3 @@
-from ttime import times
-hh = int(input("Введите кол-во часрв, которое хотите перевести:")
-         print(hh, "часов =", times(hh), "секунд.")
-         from ttime import minutes
-mm = int(input("Введите кол-во минут, которое хотите перевести: ")
-         print(mm, "минуту =", minutes(mm), "секунд.")
 
 def times(hours):
          seconds = 3600*hours
